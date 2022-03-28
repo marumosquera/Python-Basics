f799f3bb2886372775ee9a360da180ff19ee934d
marumosquera <49719332+marumosquera@users.noreply.github.com>
2022-03-28 12:03:40 -0300

    Add files via upload

---
+++ exerciseTypeConversion.py
@@ -0,0 +1,28 @@
+#booleans : True or False
+#we can use booleans to control the flow of our program
+
+name = "Elisa"
+facebook = False
+facebook = True
+
+print(bool(0))
+
+false = 0
+true = 1
+
+print(bool(1))
+
+#Facebook profile
+
+name = "Maru Mosquera"
+relationship_status = "single"
+relationship_status= 'it\'s complicated'
+
+print(relationship_status)
+
+#Let's create a program that guesses your age
+birth_year = input('What year were your born?')
+current_year= 2022
+age= current_year - int(birth_year)
+print(f'Your age is: {age} years old')
+

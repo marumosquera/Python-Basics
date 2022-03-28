fff38de7f82f716e7515d406927cef1b780eb487
marumosquera <49719332+marumosquera@users.noreply.github.com>
2022-03-28 09:13:05 -0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,26 @@
+#BestPractices - readable and understandable 
+iq = 190
+user_age = iq/4
+print(user_age)
+a,b,c = 1,2,3
+print(a)
+print(b)
+print(c)
+
+#not good practice __hi = "hi"
+
+#Constants
+PI = 3.14
+
+#expression ; piece of code that produces a Value
+#statement : entire line of code que performs some sort of action
+
+#augmented assignment operator
+some_value = 5
+some_value = 5+2
+some_value = some_value +1
+print(some_value)
+
+value = 5
+value +=2
+print(value)

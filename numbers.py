606a96649c098cd757d694033ac4fd87e353a3d7
marumosquera <49719332+marumosquera@users.noreply.github.com>
2022-03-28 00:27:37 -0300

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,23 @@
+name = input('What is your name?')
+print('Hello! My name is '+ name)
+
+#Fundamental Data Types
+
+print(type(6))
+print(type(2-4))
+print(type(0.0004))
+
+#A float takes more space in memory than an integer. Everything gets stored in 0 and 1, but a float number it's harder to represents so it gets stored in two diff locations.
+
+print(9.9+1.1)
+print(9+1)
+print(2 ** 3)
+print(2 // 4)
+#gets rounded down to a int
+
+#Modulo - reminder of the div
+print(20%4)
+
+
+
+
